f0af770985b86ab2aa6d196289763e6b319bb9fe
michaelgodley <michaelgodley@gmail.com>
2018-06-21 20:18:22 +0000

    Latest

--- services/sample/src/index.js
+++ services/sample/src/index.js
@@ -1,1 +1,34 @@
+import mongoose, { Schema } from 'mongoose';
+
 console.log('Hello World!!');
+
+// ******** Mongo ********
+const TestSchema = new Schema(
+  {
+    test: {
+      tag: {
+        type: String,
+        required: true,
+      },
+      testname: {
+        type: String,
+        lowercase: true,
+      },
+    },
+  }
+);
+
+const Test = mongoose.model('Test', TestSchema);
+
+// connect to Mongo
+// const db =
+mongoose.connect('mongodb://mongo:27017');
+
+const newTest = new Test();
+newTest.test.tag = 'abc';
+newTest.test.testname = 'def';
+newTest.save(newTest, (err) => {
+  if (err) {
+    console.log(err.message);
+  }
+});
